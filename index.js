ab0bbf72149f06fa76af9df974030a4ec4e49e38
Joe P Shoulak <joe@squarecompass.io>
2022-10-21 00:59:02 -0500

    many balls bouncing. bounce limit then death. new balls by clicking

--- index.js
+++ index.js
@@ -11,17 +11,30 @@ canvas.width = window.innerWidth;
 const colors = ["#3288F0", "#34FA99", "#B1E33B", "#FABF34", "#F04C22"];
 
 const ballRadius = 15;
+const ballCount = 500;
 
-const balls = Array(25)
+const newBall = (event) => {
+  let x, y;
+
+  if (event) {
+    x = event.x;
+    y = event.y;
+  } else {
+    x = Math.random() * (innerWidth - 2 * ballRadius) + ballRadius;
+    y = (Math.random() * innerHeight) / 2 + innerHeight / 4;
+  }
+
+  const ball = new Ball(c, x, y, ballRadius, randomFrom(colors));
+  ball.dx = Math.random() - 0.5;
+
+  return ball;
+};
+
+const balls = Array(ballCount)
   .fill()
-  .map(() => {
-    const x = Math.random() * (innerWidth - 2 * ballRadius) + ballRadius;
-    const y = (Math.random() * innerHeight) / 2 + innerHeight / 4;
+  .map(() => newBall());
 
-    const clr = randomFrom(colors);
-
-    return new Ball(c, x, y, ballRadius, clr);
-  });
+window.addEventListener("mousedown", (event) => balls.push(newBall(event)));
 
 const animate = () => {
   requestAnimationFrame(animate);
